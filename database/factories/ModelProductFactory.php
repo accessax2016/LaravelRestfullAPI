6568870e16199f54cb30e78b58d661f9d9aad048
Nguyễn Hoàng Thanh Tùng <nguyenhoangthanhtung1610@gmail.com>
2018-03-16 22:08:46 +0700

    generate data for database

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->sentence,
+        'detail' => $faker->text,
+        'price' => $faker->numberBetween(100, 1000),
+        'stock' => $faker->numberBetween(0, 100),
+        'discount' => $faker->numberBetween(5, 30),
     ];
 });
