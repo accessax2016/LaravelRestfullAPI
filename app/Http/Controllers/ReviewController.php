9aad48934e6c8b6a06c7c5106893e900063206f1
Nguyễn Hoàng Thanh Tùng <nguyenhoangthanhtung1610@gmail.com>
2018-03-17 18:45:46 +0700

    review part

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Model\Review;
 use App\Model\Product;
 use App\Http\Resources\Review\ReviewList;
+use App\Http\Requests\ReviewRequest;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -35,9 +36,13 @@ class ReviewController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ReviewRequest $request, Product $product)
     {
-        //
+        $review = new Review($request->all());
+        $product->reviews->save($review);
+        return response([
+            'data' =>  ReviewList::collection(($product)),
+        ]);
     }
 
     /**
